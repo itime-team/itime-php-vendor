e24a9f9285e95341d9c7473be6448b8c838aae66
yinchuandong <yincd520@sina.com>
2016-08-25 10:14:54 +1000

    new: complete the format of uni calendar

--- src/LibUniMelb.php
+++ src/LibUniMelb.php
@@ -25,9 +25,16 @@ class LibUniMelb implements ITimeCalendar{
         } catch (ClientException $ce){
             $statusCode = $ce->getResponse()->getStatusCode();
             $statusMsg = $ce->getResponse()->getReasonPhrase();
-            var_dump($ce);
+            return array(
+                'status' => -1,
+                'info' => $statusMsg
+                );
         } catch (Exception $e){
             // other error
+            return array(
+                'status' => -2,
+                'info' => 'server error'
+                );
         }
 
         $this->reqHeaders['body'] = json_encode([
@@ -40,12 +47,22 @@ class LibUniMelb implements ITimeCalendar{
         } catch (ClientException $ce){
             $statusCode = $ce->getResponse()->getStatusCode();
             $statusMsg = $ce->getResponse()->getReasonPhrase();
-            var_dump($ce);
+            return array(
+                'status' => -3,
+                'info' => $statusMsg
+                );
         } catch (Exception $e){
             // other error
+            return array(
+                'status' => -4,
+                'info' => 'server error'
+                );
         }
 
-        return 'login';
+        return array(
+            'status' => 1,
+            'info' => 'success'
+            );
     }
 
     public function fetch(){
@@ -57,15 +74,53 @@ class LibUniMelb implements ITimeCalendar{
         } catch (ClientException $ce){
             $statusCode = $ce->getResponse()->getStatusCode();
             $statusMsg = $ce->getResponse()->getReasonPhrase();
-            var_dump($ce);
+            return array(
+                'status' => -1,
+                'info' => $statusMsg
+                );
         } catch (Exception $e){
             // other error
-            var_dump($e);
+            return array(
+                'status' => -2,
+                'info' => 'server error'
+                );
         }
 
         $contents = $response->getBody()->getContents();
-        var_dump(json_decode($contents, true));
-        return 'fetch';
+        $jsonObj = json_decode($contents);
+        if(!property_exists($jsonObj, 'classes')){
+            return array(
+                'status' => -3,
+                'info' => 'uni server error'
+                );
+        }
+        $classesCount = count($jsonObj->classes);
+        $resultArr = [];
+        for($i = 0; $i < $classesCount; $i++){
+            $classObj = $jsonObj->classes[$i];
+            // echo $classObj->fullTitle .'\n\r';
+            $timetableCount = count($classObj->classTimetables);
+            for($j = 0; $j < $timetableCount; $j++){
+                $timetableObj = $classObj->classTimetables[$j];
+                $address = $timetableObj->locationDescription . ' Campus; ';
+                $address .= 'Building '.$timetableObj->buildingName;
+                $address .= ' ('.$timetableObj->buildingNumber.'), ';
+                $address .= 'Room '.$timetableObj->roomName;
+                $event = array(
+                    'title' => $classObj->fullTitle,
+                    'startTime' => $timetableObj->startDatetime,
+                    'endTime' => $timetableObj->endDatetime,
+                    'address' => $address
+                );
+                array_push($resultArr, $event);
+            }
+        }
+
+        return array(
+            'status' => 1,
+            'info' => 'success',
+            'data' => $resultArr
+            );
     }
 }
 
--- tests/CalendarTest.php
+++ tests/CalendarTest.php
@@ -4,17 +4,17 @@ use ITime\Calendar\ITimeFactory;
 class CalendarTest extends \PHPUnit_Framework_TestCase{
 
     public function testUniMelb(){
-        // $cls = ITimeFactory::create(ITimeFactory::$LIB_UNIMELB);
-        // $result = $cls->login('mingyanx', 'xmy15234');
-        // $result = $cls->fetch();
-        // $this->assertEquals($result, 'fetch');
+        $cls = ITimeFactory::create(ITimeFactory::$LIB_UNIMELB);
+        $ret = $cls->login('mingyanx', 'xmy15234');
+        $ret = $cls->fetch();
+        $this->assertEquals($ret['status'], 1);
     }
 
 
-    public function testMonash(){
-        $cls = ITimeFactory::create(ITimeFactory::$LIB_MONASH);
-        $result = $cls->login('xli531', 'Lx19930102');
-        $result = $cls->fetch();
-        $this->assertEquals($result, 'fetch');
-    }
+    // public function testMonash(){
+    //     $cls = ITimeFactory::create(ITimeFactory::$LIB_MONASH);
+    //     $result = $cls->login('xli531', 'Lx19930102');
+    //     $result = $cls->fetch();
+    //     $this->assertEquals($result, 'fetch');
+    // }
 }
